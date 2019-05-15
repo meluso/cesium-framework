54d4114d22779e8d079a5c735e1c50d7d5b02f6d
meluso <35809207+meluso@users.noreply.github.com>
2019-05-14 23:26:54 -0400

    Vanessa updated scripts to remove future estimate type.
    Co-Authored-By: rojanov <rojanov@users.noreply.github.com>

--- test_agent.py
+++ test_agent.py
@@ -5,7 +5,6 @@
 @name: test_agent.py
 
 This file tests the agent class.
-
 """
 
 import model_agent as ag
@@ -14,12 +13,12 @@ import matplotlib.pylab as plt
 import doe_lhs as doe
 
 # Create an instance of an agent
-a1 = ag.Agent(2,[0,3],0.5,"ackley","best_est")
-print("Location = " + str(a1.location))
-print("Neighbors = " + str(a1.neighbors))
-print("Bound = " + str(a1.obj_bounds.xmax))
-print("Estimate type = " + str(a1.est_type))
-print("Objective function type = " + a1.fn)
+a1 = ag.Agent(2,[0,3],"ackley") 
+print(("Location = " + str(a1.location)))
+print(("Neighbors = " + str(a1.neighbors)))
+print(("Bound = " + str(a1.obj_bounds.xmax)))
+print(("Estimate type = " + str(a1.est_type)))
+print(("Objective function type = " + a1.fn))
 
 # Create a history vector for a set of 4 agents
 sys_vect = [ag.Obj_Eval() for i in range(4)]
@@ -44,13 +43,12 @@ for h in range(0,101):
                 ag.Obj_Eval(rand(),rand())]
     a1.save_history(est_vect)
         
-print("History = " + str(len(a1.history)) + " points")
+print(("History = " + str(len(a1.history)) + " points"))
 
 # Set initial estimates for the agent
 print("--Initial estimate--")
 a1.initialize_estimates()
-print("Current estimate = f(" + str(a1.curr_est.x) + ") = " + str(a1.curr_est.fx))
-print("Future estimate = f(" + str(a1.hist_med.x) + ") = " + str(a1.hist_med.fx))
+print(("Current estimate = f(" + str(a1.curr_est.x) + ") = " + str(a1.curr_est.fx)))
 
 # Create an estimate
 print("--1st updated estimate--")
@@ -59,9 +57,7 @@ sys_vect = [ag.Obj_Eval(rand(),rand()),
             ag.Obj_Eval(rand(),rand()),
             ag.Obj_Eval(rand(),rand())]
 received_est = a1.generate_estimate(sys_vect)
-print("Returned estimate = f(" + str(received_est.x) + ") = " + str(received_est.fx))
-print("Current estimate = f(" + str(a1.curr_est.x) + ") = " + str(a1.curr_est.fx))
-print("Future estimate = f(" + str(a1.hist_med.x) + ") = " + str(a1.hist_med.fx))
+print(("Current estimate = f(" + str(a1.curr_est.x) + ") = " + str(a1.curr_est.fx)))
 
 # Create an estimate
 print("--2nd updated estimate--")
@@ -70,9 +66,7 @@ sys_vect = [ag.Obj_Eval(rand(),rand()),
             ag.Obj_Eval(rand(),rand()),
             ag.Obj_Eval(rand(),rand())]
 received_est = a1.generate_estimate(sys_vect)
-print("Returned estimate = f(" + str(received_est.x) + ") = " + str(received_est.fx))
-print("Current estimate = f(" + str(a1.curr_est.x) + ") = " + str(a1.curr_est.fx))
-print("Future estimate = f(" + str(a1.hist_med.x) + ") = " + str(a1.hist_med.fx))
+print(("Current estimate = f(" + str(a1.curr_est.x) + ") = " + str(a1.curr_est.fx)))
 
 # Create an estimate
 print("--3rd updated estimate--")
@@ -81,9 +75,7 @@ sys_vect = [ag.Obj_Eval(rand(),rand()),
             ag.Obj_Eval(rand(),rand()),
             ag.Obj_Eval(rand(),rand())]
 received_est = a1.generate_estimate(sys_vect)
-print("Returned estimate = f(" + str(received_est.x) + ") = " + str(received_est.fx))
-print("Current estimate = f(" + str(a1.curr_est.x) + ") = " + str(a1.curr_est.fx))
-print("Future estimate = f(" + str(a1.hist_med.x) + ") = " + str(a1.hist_med.fx))
+print(("Current estimate = f(" + str(a1.curr_est.x) + ") = " + str(a1.curr_est.fx)))
 
 x_vals = []
 fx_vals = []
