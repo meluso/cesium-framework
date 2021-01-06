87e49c2e164765cc0e101610236bf7e02b2ab6b6
meluso <35809207+meluso@users.noreply.github.com>
2021-01-06 10:33:16 -0500

    Update get_data.py

--- model/get_data.py
+++ model/get_data.py
@@ -9,10 +9,33 @@ import data_manager as dm
 import get_params as gp
 import numpy as np
 import os
+import pickle
 import sys
 
 
-def change_filenames(execnum,outputdir, run_list):
+def os_setup():
+    """Configures the function for the current operating system."""
+
+    if sys.platform.startswith('linux'):
+
+        try:
+            outputdir = str(sys.argv[1])
+            execnum = int(sys.argv[2])
+            run_list = np.arange(100)
+        except IndexError:
+            sys.exit("Usage: %s outputdir" % sys.argv[0] )
+
+    else:
+
+        outputdir = '../data/test'
+        execnum = 2
+        run_list = [999999]
+
+    return outputdir, execnum, run_list
+
+
+
+def change_filenames(outputdir, execnum, run_list):
     """Renames file case numbers to match their respective indeces."""
 
     # Get parameters for specified execution
@@ -48,9 +71,12 @@ def change_filenames(execnum,outputdir, run_list):
                 print('No ' + file_prefix)
 
 
+def run_change_filenames():
+    outputdir, execnum, run_list = os_setup()
+    change_filenames(outputdir, execnum, run_list)
 
 
-def get_incompletes(execnum, outputdir, run_list):
+def get_incompletes(outputdir, execnum, run_list):
     """Identifies runs that didn't save data for the specified execution,
     directory, and list of runs."""
 
@@ -59,13 +85,21 @@ def get_incompletes(execnum, outputdir, run_list):
 
     # Create empty list for parameter combos that didn't run
     leftovers = []
+    counts = np.zeros((len(params),2))
+    index_case = 0
+    index_count = 1
+    case_num = -1
 
     # Cycle through parameters and runs for this execution number
     for case in params:
-        for runnum in run_list:
 
-            # Get case for testing
-            casenum = case['ind']
+        case_num += 1
+
+        # Get case for testing
+        casenum = case['ind']
+        counts[case_num,index_case] = casenum
+
+        for runnum in run_list:
 
             # Add run number to case info
             case['run'] = runnum
@@ -78,27 +112,34 @@ def get_incompletes(execnum, outputdir, run_list):
             # Check if the file exists
             try:
                 summary, history = dm.load_data(file_prefix)
+                counts[case_num,index_count] += 1
             except IOError:
-                leftovers.append(case)
+                leftovers.append(case.copy())
 
-    return leftovers
+    return leftovers, counts
+
+
+def run_get_incompletes():
+    outputdir, execnum, run_list = os_setup()
+    leftovers, counts = get_incompletes(outputdir, execnum, run_list)
+    pickle.dump(leftovers, open(f'leftovers_exec{execnum:03}.pickle',"wb"))
+    np.save(f'counts_exec{execnum:03}.npy',counts)
+
+
+def load_incompletes(execnum):
+    """Loads the incompletes from file."""
+
+    if sys.platform.startswith('linux'):
+        save_dir = ''
+    else:
+        save_dir = '../data/leftovers/'
+
+    return pickle.load(open(save_dir + f'leftovers_exec{execnum:03}.pickle',
+                            "rb"))
 
 
 if __name__ == '__main__':
+    run_get_incompletes()
 
-    if sys.platform.startswith('linux'):
 
-        try:
-            outputdir = str(sys.argv[1])
-            execnum = int(sys.argv[2])
-            runlist = np.arange(100)
-        except IndexError:
-            sys.exit("Usage: %s outputdir" % sys.argv[0] )
 
-    else:
-
-        outputdir = '../data/test'
-        execnum = 2
-        runlist = [999999]
-
-    change_filenames(execnum, outputdir, runlist)
